02a0fe4f6ca269aa3dc5ab9e66b25e54bf804a28
danidimarti <dani.dimarti@gmail.com>
2023-01-24 16:48:02 +0100

    done plus bonus

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,103 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.randint(1, 10,size=(3,4,3))
+a_2 = np.random.rand(3,4,3)
 
 #4. Print a.
 
-
+a
+a_2
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
 
-
+b
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+#two ways:
+    def numpy_info(array_name):
+    print(f"size is {array_name.size}")
+    print(f"shape is {array_name.shape}")
+    print(f"dimension is {array_name.ndim}")
+ #OR
 
-
+def compare(a,b):
+    if a.shape == b.shape:
+        print(f"they are the same shape:{a.shape}")
+    elif a.shape != b.shape:
+        print(f"NOPE a is {a.shape} and b is {b.shape}")
+compare(a,b)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+No, because they have different shapes. They are incompatible.
+
+"ValueError: operands could not be broadcast together with shapes (3,4,3) (5,2,3)" 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.transpose((1,2,0))
 
+c.shape == a.shape
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = a + c
+
+It works now because they have the same shape. Now they can match the colums and rows to make the operation. 
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
+
+Since array c is a tensor of ones the operation a + c would result on every value of the tensor a to be incremented by one. this would be the same as a+=1
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e = a * c
+
 
 
 #13. Does e equal to a? Why or why not?
 
+a == e
 
+np.array_equal(a,e)
+
+because array_equal method compares two arrays and returns True if they have same shape and elements, False otherwise.
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty((2,3,5))
 
 
 
